337bb42ccb2d573297ae0cc7132bf5ebc67f7b64
nobomooooon <nobomooooon>
2017-03-01 22:37:57 +0900

    サンプル導入

---
+++ sample/src/sample.c
@@ -0,0 +1,46 @@
+
+#include <stdio.h>
+
+static int sample(void);
+void hoge(void name(/* arguments */) {
+    /* code */
+})
+
+int main(void){
+	printf("Hello World\n");
+
+	for(int i=0;i<3;i++){
+		printf("goto\n");
+
+		sample();
+
+	}
+	return 0;
+
+}
+static int sample(void){
+	return 5;
+};
+
+void hoge( void ){
+
+  int b;
+
+  int hoge;
+
+  int hogehoge;
+
+
+  b = 789;
+
+  hoge = 987;
+  int hoge3;
+
+  hoge3 = b * hoge;
+
+
+
+  hogehoge = b + hoge
+
+
+};
